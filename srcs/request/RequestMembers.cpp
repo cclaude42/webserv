4a4b5060e95d6332c93f4cdb6e8dde0d05efcd70
hbaudet <hbaudet@student.42.fr>
2021-03-29 23:14:51 +0200

    I, mighty hbaudet, declare this project good enough

--- srcs/request/RequestMembers.cpp
+++ srcs/request/RequestMembers.cpp
@@ -6,7 +6,7 @@
 /*   By: hbaudet <hbaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 17:10:06 by hbaudet           #+#    #+#             */
-/*   Updated: 2021/03/29 22:32:28 by hbaudet          ###   ########.fr       */
+/*   Updated: 2021/03/29 22:50:31 by hbaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -180,7 +180,6 @@ int					Request::parse(const std::string& str)
 	this->setLang();
 	this->setBody(str.substr(i, std::string::npos));
 	this->findQuery();
-	std::cerr << RED << "Host : " << this->_headers["Host"] << RESET << '\n';
 	return this->_ret;
 }
 
